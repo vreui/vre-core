13e05fa54f3e5c993e72e936608ec00c806983fd
sceext <sceext@foxmail.com>
2022-09-09 14:58:24 +0800

    初步添加依赖

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vre-core
+<https://github.com/vreui/vre-core>
+
 威惹: 核心引擎
+
+TODO
+
+
+## LICENSE
+
+`Mozilla Public License 2.0`
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//! vrecore: 威惹 核心引擎 (WebRender)
+
+#![deny(unsafe_code)]
+
+// 模块
+// TODO
+
+// 导出
+// TODO
+
+// 全局测试
+#[cfg(test)]
+mod test;
+
+// TODO 临时代码
+// 威惹引擎 启动入口
+pub fn 启动引擎() {
+    // TODO
+    println!("vrecore::启动引擎");
+}
